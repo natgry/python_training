de8a2e75f04364b3a36ae6bf02c03a3b9387bc1c
Gryaznova Natalia <natgry@yandex.ru>
2016-12-02 15:46:11 +0300

    Добавлен новый тестовый метод test_add_empty_group()

--- test_add_group.py
+++ test_add_group.py
@@ -16,12 +16,21 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_test_add_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="qw", header="qw", footer="qw")
+        self.create_group(wd, name="group1", header="header1", footer="footer1")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
