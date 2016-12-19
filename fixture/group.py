a8e29454b1301d66da6a2fdd70ee62c819402c3a
Gryaznova Natalia <natgry@yandex.ru>
2016-12-20 01:04:52 +0300

    Добавлена конструкция if-else для fill_contact_form

--- fixture/group.py
+++ fixture/group.py
@@ -42,7 +42,6 @@ class GroupHelper:
         self.return_to_groups_page()
 
     def fill_group_form(self, group):
-        wd = self.app.wd
         self.change_field_value("group_name", group.name)
         self.change_field_value("group_header", group.header)
         self.change_field_value("group_footer", group.footer)
