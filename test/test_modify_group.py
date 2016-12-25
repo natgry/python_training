4c7a6e37ab24b234012b6e1875844c390f1b3083
Gryaznova Natalia <natgry@yandex.ru>
2016-12-25 22:16:43 +0300

    Добавлены проверки в тесты о количестве групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,24 +3,36 @@ from model.group import Group
 
 
 def test_modify_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="header", footer="footer"))
     app.group.modify_first_group(Group(name="", header="header_upd", footer="footer_upd"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="header"))
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_footer(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", footer="footer"))
     app.group.modify_first_group(Group(footer="New footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
