4915c4021529b78fa183fc11f981f7a875a319fa
Gryaznova Natalia <natgry@yandex.ru>
2016-12-25 23:13:08 +0300

    Добавили сортировку групп в тесты

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,22 +2,17 @@
 from model.group import Group
 
 
-def test_modify_group(app):
-    old_groups = app.group.get_group_list()
-    if app.group.count() == 0:
-        app.group.create(Group(name="test", header="header", footer="footer"))
-    app.group.modify_first_group(Group(name="", header="header_upd", footer="footer_upd"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-
-
 def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    app.group.modify_first_group(Group(name="New group"))
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
+    old_groups[0] = group
     assert len(old_groups) == len(new_groups)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
@@ -34,5 +29,14 @@ def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", footer="footer"))
     app.group.modify_first_group(Group(footer="New footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
+
+def test_modify_group(app):
+    old_groups = app.group.get_group_list()
+    if app.group.count() == 0:
+        app.group.create(Group(name="test", header="header", footer="footer"))
+    app.group.modify_first_group(Group(name="", header="header_upd", footer="footer_upd"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
