3b89a3c8d241b384280fb8a61b87f48a2f6e0535
Gryaznova Natalia <natgry@yandex.ru>
2016-12-26 03:05:33 +0300

    Реализовано кеширование и хэширование при загрузке групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,34 +9,31 @@ def test_modify_group_name(app):
     group = Group(name="New group")
     group.id = old_groups[0].id
     app.group.modify_first_group(group)
-    new_groups = app.group.get_group_list()
     old_groups[0] = group
-    assert len(old_groups) == len(new_groups)
+    assert len(old_groups) == app.group.count()
+    new_groups = app.group.get_group_list()
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
-    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="header"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    assert len(old_groups) == app.group.count()
 
 
 def test_modify_group_footer(app):
-    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", footer="footer"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(footer="New footer"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    assert len(old_groups) == app.group.count()
 
 
 def test_modify_group(app):
-    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="header", footer="footer"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="", header="header_upd", footer="footer_upd"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    assert len(old_groups) == app.group.count()
