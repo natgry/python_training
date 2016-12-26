22f893aa9689fa38e403d4900eb48282d2ded191
Gryaznova Natalia <natgry@yandex.ru>
2016-12-26 03:32:51 +0300

    Доработали тесты для удаления и модификации случано выбранных групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from random import randrange
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="New group")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
-    old_groups[0] = group
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
+    old_groups[index] = group
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
