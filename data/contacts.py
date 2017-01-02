6f4f5f8b171c2fad1b31ee0128ef4d354f648646
Gryaznova Natalia <natgry@yandex.ru>
2017-01-02 18:34:38 +0300

    Реализовали динамическое связывание тестовых данных с тестовыми функциями

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="name1", lastname="lastname1", middlename="middlename1"),
+    Contact(firstname="name2", lastname="lastname2", middlename="middlename2"),
+]
+
+
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from data.add_contact import testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+# @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+# def test_add_contact(app, contact):
+#     old_contacts = app.contact.get_contact_list()
+#     app.contact.create(contact)
+#     assert len(old_contacts) + 1 == app.contact.count()
+#     new_contacts = app.contact.get_contact_list()
+#     old_contacts.append(contact)
+#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 # def test_add_contact_with_spaces(app):
