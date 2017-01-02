34d4080a4e8a3e9eed02dbcad07b38af422d69d1
Gryaznova Natalia <natgry@yandex.ru>
2017-01-02 18:55:43 +0300

    Реализовать загрузку тестовых данных из файлов в формате json при помощи hook-функции pytest_generate_tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,6 +12,16 @@ def test_add_contact(app, data_contacts):
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
+    old_contacts = app.contact.get_contact_list()
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
 # @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 # def test_add_contact(app, contact):
 #     old_contacts = app.contact.get_contact_list()
