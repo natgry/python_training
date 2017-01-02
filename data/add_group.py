e0a0fb1c6d1fb8e1e52cb2e3ad547975b5a395a7
Gryaznova Natalia <natgry@yandex.ru>
2017-01-02 17:36:40 +0300

    Создали пакет для тестовых данных создания контактов

--- data/add_group.py
+++ data/add_group.py
@@ -8,6 +8,7 @@ constant = [
     Group(name='name2', header='header2', footer='footer2')
 ]
 
+
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,33 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_phone(maxlen):
-    symbols = "123456789"
-    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "-" + \
-           "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_email(maxlen):
-    letters = string.ascii_letters
-    return "".join([random.choice(letters) for i in range(random.randrange(maxlen))]) + "@" + \
-           "".join([random.choice(letters) for i in range(random.randrange(maxlen))]) + ".ru"
-
-
-testdata = [Contact(firstname="", lastname="", middlename="")] + [
-    Contact(firstname=random_string("firstname_", 10), middlename=random_string("middlename_", 20), lastname=random_string("lastname_", 20),
-            homephone=random_phone(5), mobilephone=random_phone(5), workphone=random_phone(5), phone2=random_phone(5),
-            address=random_string("address", 20), email=random_email(8), email2=random_email(8), email3=random_email(8))
-    for i in range(0, 5)
-]
+from data.add_contact import testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
