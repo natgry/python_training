e512feebcacb436bb0c6c67afc9cb491319182b8
Gryaznova Natalia <natgry@yandex.ru>
2017-01-11 02:05:43 +0300

    Добавили тест для удаления контакта с проверками из базы данных

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,8 +1,22 @@
 from model.contact import Contact
-from random import randrange
+import random
 
 
-def test_delete_some_contact(app):
+def test_delete_some_contact_by_id(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="natalia", lastname="gryaznova", homephone="123-456",
+                                   mobilephone="123-456", workphone="123-456", phone2="123-456"))
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    app.contact.delete_contact_by_id(contact.id)
+    new_contacts = db.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts.remove(contact)
+    assert old_contacts == new_contacts
+
+
+def test_delete_some_contact_by_index(app):
+    from random import randrange
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="natalia", lastname="gryaznova", homephone="123-456",
                                    mobilephone="123-456", workphone="123-456", phone2="123-456"))
