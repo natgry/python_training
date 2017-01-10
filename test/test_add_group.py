ffca6c05fd6127143b8d166775ec68d3046ae9ba
Gryaznova Natalia <natgry@yandex.ru>
2017-01-11 02:06:20 +0300

    Косметический апдейт тестов для добавления и удаления групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,7 @@ def test_add_group_from_json_db_check(app, db, json_groups):
     group = json_groups
     old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
+    assert len(old_groups) + 1 == len(db.get_group_list())
     new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
