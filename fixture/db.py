9bcceb2d933b07ba1e2c5e7c8e4f5a88d4817e33
Gryaznova Natalia <natgry@yandex.ru>
2017-01-11 01:42:26 +0300

    Добавили метод get_contact_list для получения контактов из базы данных

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -23,18 +24,18 @@ class DbFixture:
             cursor.close()
         return list
 
-    # def get_contact_list(self):
-    #     list = []
-    #     cursor = self.connection.cursor()
-    #     try:
-    #         # cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
-    #         for row in cursor:
-    #             (id, name, header, footer) = row
-    #             # list.append(Group(id=str(id), name=name, header=header, footer=footer))
-    #             list.append(Contact())
-    #     finally:
-    #         cursor.close()
-    #     return list
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, email, home, address, address2 from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (user_id, firstname, lastname, email, homephone, address, address2) = row
+                list.append(Contact(id=str(user_id), firstname=firstname, lastname=lastname,
+                                    email=email, homephone=homephone, address=address, address2=address2))
+        finally:
+            cursor.close()
+        return list
 
     def destroy(self):
         self.connection.close()
