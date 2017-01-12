76d337a7f5fa0dd5b4953a4113344e2f2420b084
Gryaznova Natalia <natgry@yandex.ru>
2017-01-12 22:04:22 +0300

    Переделали тесты для модификации групп с подгрузкой списка групп из базы данных
    Также реализовали отключаемые проверки пользовательского интерфейса --
    сравнение данных, полученных из пользовательского интерфейса, с данными,
    загруженными из базы данных.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from random import randrange
+import random
+
+
+def test_modify_group_by_id_with_db_check(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="test"))
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    new_group = Group(name="New group", header="new header", footer="new footer")
+    new_group.id = group.id
+    app.group.modify_group_by_id(group.id, new_group)
+    old_groups = [new_group if x.id == group.id else x for x in old_groups]
+    new_groups = db.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 def test_modify_some_group_name(app):
+    from random import randrange
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
