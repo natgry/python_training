4dcd9aabf617d7ea488efa91a9e9c2e240f64b01
Gryaznova Natalia <natgry@yandex.ru>
2017-01-12 03:03:09 +0300

    Добавили паузу в тест удаления контакта с последующей проверкой в базе данных
    После удаления контакта и следующим обращением к базе данных, база
    данных показывает, что контакт есть. Решение: после удаления контакта
    нужно подождать некоторое время, пока база данных обновится.

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -8,7 +8,7 @@ def test_group_list(app, db):
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
     print(timeit(lambda: map(clean, db.get_group_list()), number=1))
-    assert False
+    # assert False # to see the timeit info
     # assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
 
 #
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,18 +1,22 @@
 from model.contact import Contact
 import random
+from time import sleep
 
 
-def test_delete_some_contact_by_id(app, db):
+def test_delete_some_contact_by_id(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="natalia", lastname="gryaznova", homephone="123-456",
                                    mobilephone="123-456", workphone="123-456", phone2="123-456"))
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
     app.contact.delete_contact_by_id(contact.id)
+    sleep(0.5)  # wait till db updates contacts records after deletion
     new_contacts = db.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
 
 def test_delete_some_contact_by_index(app):
