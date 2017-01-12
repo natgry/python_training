9c29512afd6c8468a699d1ac10e924ea4a4d3180
Gryaznova Natalia <natgry@yandex.ru>
2017-01-12 22:20:01 +0300

    Переделаны тесты для добавления контактов с подгрузкой списков объектов…
    А также реализованы отключаемые проверки пользовательского интерфейса --
    сравнение данных, полученных из пользовательского интерфейса, с данными,
    загруженными из базы данных.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,8 +2,20 @@
 from model.contact import Contact
 
 
-def test_add_contact(app, data_contacts):
-    contact = data_contacts
+def test_add_contact_from_json_with_db_check(app, db, json_contacts, check_ui):
+    contact = json_contacts
+    old_contacts = db.get_contact_list()
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == len(db.get_contact_list())
+    new_contacts = db.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+
+
+def test_add_contact_from_json_ui_check(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
@@ -12,18 +24,8 @@ def test_add_contact(app, data_contacts):
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_add_contact_from_json_db_check(app, db, json_contacts):
-    contact = json_contacts
-    old_contacts = db.get_contact_list()
-    app.contact.create(contact)
-    assert len(old_contacts) + 1 == len(db.get_contact_list())
-    new_contacts = db.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_contact_from_json_ui_check(app, json_contacts):
-    contact = json_contacts
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
